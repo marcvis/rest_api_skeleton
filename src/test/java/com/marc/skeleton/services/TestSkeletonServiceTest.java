7a2e85c515b28c9e01363a641c772e8532bc5304
monkeySee-monkeyCode <charly.dont.surf@gmail.com>
2015-12-28 01:45:40 -0500

    added unit test cycle

--- src/test/java/com/marc/skeleton/services/TestSkeletonServiceTest.java
+++ src/test/java/com/marc/skeleton/services/TestSkeletonServiceTest.java
@@ -1,3 +1,30 @@
+package com.marc.skeleton.services;
+
+import com.marc.skeleton.models.SkeletonModel;
+import com.marc.skeleton.services.SkeletonService;
+import com.marc.skeleton.services.TestSkeletonService;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class TestSkeletonServiceTest {
+    SkeletonService subject;
+    @Before
+    public void setup(){
+
+        subject = new TestSkeletonService();
+    }
+    @Test
+    public void saveSkeletonShouldBeRetrievable(){
+        //Given
+        SkeletonModel expectedModel = new SkeletonModel(42,"testModel","broken");
+        subject.createSkeletonModel(expectedModel);
+        //When
+        SkeletonModel actualModel = subject.getSkeletonModel(42);
+        //Then
+        assertEquals(expectedModel, actualModel);
+
+    }
 
 }
