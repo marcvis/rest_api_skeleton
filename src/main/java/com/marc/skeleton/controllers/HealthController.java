7ed234c75a1b19cee18b90d323d47baa4a541193
marcvis <marcdvis@gmail.com>
2015-12-29 16:39:39 -0800

    Added healthcheck and integration test phase

---
+++ src/main/java/com/marc/skeleton/controllers/HealthController.java
@@ -0,0 +1,17 @@
+package com.marc.skeleton.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public void getHealth() {
+    }
+}
